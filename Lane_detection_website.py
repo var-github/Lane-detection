29466b2f609d7193cdc2040740d12095e3c2870d
Varun R <64779815+var-github@users.noreply.github.com>
2025-09-05 19:21:25 +0530

    Update Lane_detection_website.py

--- Lane_detection_website.py
+++ Lane_detection_website.py
@@ -91,7 +91,7 @@ if video_file is not None:
             stop()
 
         # 'avc1' codec for H.264 in MP4 container
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
         # Output temp file
         st.session_state['input_processed'] = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4").name
@@ -140,6 +140,7 @@ if video_file is not None:
 
         st.text(os.path.abspath(st.session_state['input_processed']))
         st.text(os.path.abspath(st.session_state['output_processed']))
+
 
 
 
