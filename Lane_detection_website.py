9e76ee54d6489b5a0c2006a8e7c68eaa0970850a
Varun R <64779815+var-github@users.noreply.github.com>
2025-09-14 11:03:21 +0530

    Update Lane_detection_website.py

--- Lane_detection_website.py
+++ Lane_detection_website.py
@@ -116,7 +116,7 @@ st.markdown("""
     height: 80px;
     width: 80px;
     border: none;
-    font-size: 16px;
+    font-size: 25px;
 }
 </style>""", unsafe_allow_html=True)
 
@@ -157,6 +157,7 @@ if video_file is not None:
         st.video(format_video(input_processed))
         st.subheader("Processed Video")
         st.video(format_video(output_processed))
+
 
 
 
